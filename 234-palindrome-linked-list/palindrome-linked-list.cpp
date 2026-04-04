54c06364039cf185093318f0dada674aadf53d6e
Priyanshu Gupta <pg747974@gmail.com>
2026-04-04 11:54:42 +0530

    Time: 2 ms (63.15%) | Memory: 121.1 MB (48.23%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -10,23 +10,34 @@
  */
 class Solution {
 public:
+
+    ListNode* reverseLL(ListNode *head){
+     if (head == NULL || head->next == NULL) return head;
+     ListNode* newHead = reverseLL(head->next);
+     ListNode* front = head->next;
+     front->next = head;
+     head->next = NULL;
+     return newHead;
+
+    }
     bool isPalindrome(ListNode* head) {
-        stack< int > s;
-        ListNode* temp= head;
-        while(temp!=NULL){
-            s.push(temp->val);
-            temp=temp->next;
+        ListNode*slow=head;
+        ListNode*fast=head;
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        ListNode* temp2=head;
-        while(temp2!=NULL){
-            if(s.top()==temp2->val){
-                temp2=temp2->next;
-                s.pop();
-            }
-            else{
+        ListNode* newhead = reverseLL(slow->next);
+        ListNode*first=head;
+        ListNode*second= newhead;
+        while(second!=NULL){
+            if(first->val!=second->val){
                 return false;
             }
+            first=first->next;
+            second=second->next;
         }
+        reverseLL(newhead);
         return true;
     }
 };
