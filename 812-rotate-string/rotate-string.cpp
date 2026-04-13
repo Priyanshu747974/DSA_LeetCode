3b11d5cfd0355569ddbad82e2395ffc6a6626cf2
Priyanshu Gupta <pg747974@gmail.com>
2026-04-13 23:51:56 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (84.66%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size()!=goal.size())return false;
+        for(int i=0;i<s.size();i++){
+            char ch=s[0];
+            s.erase(0,1);
+            s.push_back(ch);
+            if(s==goal)return true;
+        }
+        return false;
+    }
+};
