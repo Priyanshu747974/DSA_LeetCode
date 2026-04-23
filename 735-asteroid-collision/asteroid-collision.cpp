a33e2c9542b971819319d4e5b8c70565cf603dac
Priyanshu Gupta <pg747974@gmail.com>
2026-04-23 09:48:03 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (70.08%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector <int> v;
+        for(int i=0;i<asteroids.size();i++){
+            if(asteroids[i]>0){
+                v.push_back(asteroids[i]);
+            }
+            else{
+                while(!v.empty() && v.back()>0 && v.back()<abs(asteroids[i])){
+                    v.pop_back();
+                }
+                if(!v.empty() && v.back()==abs(asteroids[i])){
+                    v.pop_back();
+                }
+                else if(v.empty() || v.back()<0){
+                    v.push_back(asteroids[i]);
+                }
+            }
+        }
+        return v;
+    }
+};
