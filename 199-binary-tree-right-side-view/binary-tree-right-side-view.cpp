77c88faf66c15ff37e2e45620991f6e53b166880
Priyanshu Gupta <pg747974@gmail.com>
2026-05-25 12:18:18 +0530

    Time: 7 ms (0.41%) | Memory: 17.8 MB (6.75%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,38 +11,21 @@
  */
 class Solution {
 public:
-    vector<vector<int>> function(TreeNode* root){
-        vector<vector<int>> ans;
+    vector<int> function(TreeNode* root, int  level,vector<int> & ans){
         if(root==NULL){
             return ans;
         }
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            int size=q.size();
-            vector<int> level;
-            for(int i=0;i< size;i++){
-                TreeNode* node=q.front();
-                q.pop();
-                if(node->left!=NULL){
-                    q.push(node->left);
-                }
-                if(node->right!=NULL){
-                    q.push(node->right);
-                }
-                level.push_back(node->val);
-            }
-            ans.push_back(level);
+        if(level==ans.size()){
+            ans.push_back(root->val);
         }
+        function(root->right,level+1,ans);
+        function(root->left,level+1,ans);
         return ans;
     }
     vector<int> rightSideView(TreeNode* root) {
-        vector<vector<int>> ans;
-        ans= function(root);
-        vector<int> v;
-        for(int i=0;i<ans.size();i++){
-            v.push_back(ans[i][ans[i].size()-1]);
-        }
-        return v;
+        vector<int> ds;
+        int level=0;
+        function(root,level,ds);
+        return ds;
     }
 };
