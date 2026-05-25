91ce3d8582ccbd8048c883de0370096bcad76c9a
Priyanshu Gupta <pg747974@gmail.com>
2026-05-25 23:34:44 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (25.86%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -11,50 +11,23 @@
  */
 class Solution {
 public:
-    TreeNode* checkInversion(TreeNode* root){
-        if(root==NULL){
-            return NULL;
-        }
-        TreeNode* left= checkInversion(root->left);
-        TreeNode* right= checkInversion(root->right);
-
-        root->left=right;
-        root->right=left;
-
-        return root;
-    }
-    TreeNode* createDuplicate(TreeNode* root){
-        if(root==NULL){
-            return NULL;
-        }
-
-        TreeNode* newNode = new TreeNode(root->val);
-
-        newNode->left = createDuplicate(root->left);
-        newNode->right = createDuplicate(root->right);
-
-        return newNode;
-    }
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==NULL && q==NULL){
+    bool isMirror(TreeNode* left, TreeNode* right){
+        if(left==NULL && right==NULL){
             return true;
         }
-        if(p!=NULL && q==NULL){
+        if(left==NULL || right==NULL){
             return false;
         }
-        if(p==NULL && q!=NULL){
+        if(left->val!=right->val){
             return false;
         }
-        if(p->val == q->val){
-            return isSameTree(p->left,q->left) &&
-            isSameTree(p->right,q->right);
-        }
-        return false;
+        return isMirror(left->left,right->right)&&
+        isMirror(left->right,right->left);
     }
-
     bool isSymmetric(TreeNode* root) {
-        TreeNode* tree =createDuplicate(root);
-        checkInversion(root);
-        return isSameTree(root,tree);
+        if(root==NULL){
+            return true;
+        }
+        return isMirror(root->left,root->right);
     }
 };
