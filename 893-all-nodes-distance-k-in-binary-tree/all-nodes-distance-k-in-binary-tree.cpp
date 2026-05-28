f8fe954e1109c915ea444afd1a12f17f7fc9e61e
Priyanshu Gupta <pg747974@gmail.com>
2026-05-28 23:37:58 +0530

    Time: 4 ms (66.19%) | Memory: 15.3 MB (69.29%) - LeetSync

---
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,106 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    void markParents(TreeNode* root,
+                     unordered_map<TreeNode*, TreeNode*>& parent_track) {
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* current = q.front();
+            q.pop();
+
+            if (current->left) {
+                parent_track[current->left] = current;
+                q.push(current->left);
+            }
+
+            if (current->right) {
+                parent_track[current->right] = current;
+                q.push(current->right);
+            }
+        }
+    }
+
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+
+        unordered_map<TreeNode*, TreeNode*> parent_track;
+
+        // Store parent of every node
+        markParents(root, parent_track);
+
+        unordered_map<TreeNode*, bool> visited;
+
+        queue<TreeNode*> q;
+        q.push(target);
+
+        visited[target] = true;
+
+        int curr_level = 0;
+
+        // BFS from target node
+        while (!q.empty()) {
+
+            int size = q.size();
+
+            // Reached required distance
+            if (curr_level == k) {
+                break;
+            }
+
+            curr_level++;
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode* current = q.front();
+                q.pop();
+
+                // Left child
+                if (current->left &&
+                    !visited[current->left]) {
+
+                    q.push(current->left);
+                    visited[current->left] = true;
+                }
+
+                // Right child
+                if (current->right &&
+                    !visited[current->right]) {
+
+                    q.push(current->right);
+                    visited[current->right] = true;
+                }
+
+                // Parent node
+                if (parent_track[current] &&
+                    !visited[parent_track[current]]) {
+
+                    q.push(parent_track[current]);
+                    visited[parent_track[current]] = true;
+                }
+            }
+        }
+
+        vector<int> result;
+
+        // Nodes present in queue are at distance K
+        while (!q.empty()) {
+            TreeNode* current = q.front();
+            q.pop();
+
+            result.push_back(current->val);
+        }
+
+        return result;
+    }
+};
