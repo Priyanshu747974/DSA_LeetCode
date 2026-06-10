9902fe5a679671b3366f41a5caaeed858ce88747
Priyanshu Gupta <pg747974@gmail.com>
2026-06-10 23:04:20 +0530

    Time: 3 ms (75.30%) | Memory: 20.3 MB (17.87%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -3,46 +3,45 @@ public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n = numCourses;
 
-        vector<int> adj[n];
-        for(auto it : prerequisites){
+        vector<vector<int>> adj(n);
+
+        for (auto it : prerequisites) {
             adj[it[1]].push_back(it[0]);
         }
 
         vector<int> indegree(n, 0);
 
-        for(int i = 0; i < n; i++){
-            for(auto it : adj[i]){
+        for (int i = 0; i < n; i++) {
+            for (auto it : adj[i]) {
                 indegree[it]++;
             }
         }
 
         queue<int> q;
-        for(int i = 0; i < n; i++){
-            if(indegree[i] == 0){
+
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
                 q.push(i);
             }
         }
 
         vector<int> topo;
 
-        while(!q.empty()){
+        while (!q.empty()) {
             int node = q.front();
             q.pop();
 
             topo.push_back(node);
 
-            for(auto it : adj[node]){
+            for (auto it : adj[node]) {
                 indegree[it]--;
 
-                if(indegree[it] == 0){
+                if (indegree[it] == 0) {
                     q.push(it);
                 }
             }
         }
 
-        if(topo.size() == n){
-            return true;
-        }
-        return false;
+        return topo.size() == n;
     }
 };
