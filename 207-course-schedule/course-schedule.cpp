c66d8146eedc27228bba19e9ae7e47f95ea9476b
Priyanshu Gupta <pg747974@gmail.com>
2026-06-11 19:54:44 +0530

    Time: 7 ms (45.85%) | Memory: 20.2 MB (24.91%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,5 +1,33 @@
 class Solution {
 public:
+    vector<int> toposort(vector<vector<int>>& adj,int v){
+        vector<int> indegree(v,0);
+        for(int i=0;i<v;i++){
+            for(auto it : adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<v;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for(auto it: adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        return topo;
+    }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n = numCourses;
 
@@ -8,40 +36,10 @@ public:
         for (auto it : prerequisites) {
             adj[it[1]].push_back(it[0]);
         }
-
-        vector<int> indegree(n, 0);
-
-        for (int i = 0; i < n; i++) {
-            for (auto it : adj[i]) {
-                indegree[it]++;
-            }
+        vector<int> topo=toposort(adj,n);
+        if(topo.size()==n){
+            return true;
         }
-
-        queue<int> q;
-
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) {
-                q.push(i);
-            }
-        }
-
-        vector<int> topo;
-
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-
-            topo.push_back(node);
-
-            for (auto it : adj[node]) {
-                indegree[it]--;
-
-                if (indegree[it] == 0) {
-                    q.push(it);
-                }
-            }
-        }
-
-        return topo.size() == n;
+        return false;
     }
 };
