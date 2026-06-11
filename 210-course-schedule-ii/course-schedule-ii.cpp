50e1ca49ab15303fec8b7dec2955ed8408572466
Priyanshu Gupta <pg747974@gmail.com>
2026-06-11 20:43:35 +0530

    Time: 5 ms (43.19%) | Memory: 19 MB (20.42%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> toposort(vector<vector<int>>& adj,int v){
+        vector<int> indegree(v,0);
+        for(int i=0;i<v;i++){
+            for(auto it : adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int> q;
+        for(int i=0;i<v;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+            
+        }
+        vector<int> topo;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it:adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        return topo;
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        int n=numCourses;
+        vector<vector<int>> adj(numCourses);
+        for (auto it : prerequisites) {
+            adj[it[1]].push_back(it[0]);
+        }
+        vector<int> topo = toposort(adj, numCourses);
+
+        if(topo.size() != numCourses){
+          return {};
+        }
+        return topo;
+    }
+};
