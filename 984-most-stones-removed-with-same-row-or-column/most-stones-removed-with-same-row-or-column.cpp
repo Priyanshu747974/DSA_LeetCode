bc5bae6ef7c59f1b07a980d38af38b64edb7da87
Priyanshu Gupta <pg747974@gmail.com>
2026-07-05 20:41:10 +0530

    Time: 131 ms (5.07%) | Memory: 18.3 MB (90.77%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,57 @@
+class DisjointSet {
+    vector<int>parents, rnk;
+public:
+    DisjointSet(int n){
+        rnk.resize(n+1,0);
+        parents.resize(n+1);
+        for(int i=0;i<n+1;i++){
+            parents[i]=i;
+        }
+    }
+    int findULT(int node){
+        if(node==parents[node]){
+            return node;        
+        }
+        return parents[node]=findULT(parents[node]);
+    }
+    void unionBYrank(int u, int v){
+        int ulp_u = findULT(u);
+        int ulp_v = findULT(v);
+
+        if(ulp_u == ulp_v){
+            return;
+        }
+        if(rnk[ulp_u] < rnk[ulp_v]){
+            parents[ulp_u]=ulp_v;
+        }
+        else if(rnk[ulp_u] > rnk[ulp_v]){
+            parents[ulp_v]=ulp_u;
+        }
+        else{
+            parents[ulp_v]=ulp_u;
+            rnk[ulp_u]++;
+        }
+    }
+};
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        int components=n;
+        DisjointSet ds(n);
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int ui = ds.findULT(i);
+                int uj = ds.findULT(j);
+                if(stones[i][0]==stones[j][0] || stones[i][1] == stones[j][1]){
+                    if(ui==uj){
+                        continue;
+                    }
+                    ds.unionBYrank(i,j);
+                    components--;
+                }
+            }
+        }
+        return n-components;
+    }
+};
