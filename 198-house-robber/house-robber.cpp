1a03ae05ad52d518290a2b215e1f94f0c0c65cbf
Priyanshu Gupta <pg747974@gmail.com>
2026-07-10 21:37:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (64.15%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int function(int ind, vector<int>& nums, vector<int>&dp){
+        int n = nums.size();
+        if (ind == 0){
+            return nums[0];
+        }
+        if(ind<0){
+            return 0;
+        }    
+        if(dp[ind]!= -1){
+            return dp[ind];
+        }
+        int pick = nums[ind]+ function(ind-2,nums,dp);
+        int not_pick = function(ind-1,nums,dp);
+
+        return dp[ind]=max(pick, not_pick);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,-1);
+        return function(n-1,nums,dp);
+    }
+};
