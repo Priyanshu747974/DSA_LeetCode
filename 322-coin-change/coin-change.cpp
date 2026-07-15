91ac137f7b7c72bfc5204965783775f993ac9638
Priyanshu Gupta <pg747974@gmail.com>
2026-07-15 20:32:11 +0530

    Time: 46 ms (36.70%) | Memory: 48.8 MB (44.37%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -5,8 +5,8 @@ public:
             return 0;
         }
         if(ind == 0){
-            if(amount%coins[ind]==0){
-                return amount/coins[ind];
+            if(amount%coins[0]==0){
+                return amount/coins[0];
             }
             return 1e9;
         }
@@ -23,10 +23,30 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         vector<vector<int>> dp(coins.size(),vector<int>(amount+1,-1));
-        int ans = function(coins.size()-1,coins,amount,dp);
-        if(ans>=1e9){
+        for(int i = 0;i<coins.size();i++){
+            dp[i][0]=0;
+        }
+        for(int amt=0;amt<amount+1;amt++){
+            if(amt%coins[0]==0){
+                dp[0][amt]=amt/coins[0];
+            }
+            else{
+                dp[0][amt] = 1e9;
+            }
+        }
+        for(int ind = 1;ind<coins.size();ind++){
+            for(int amt=1;amt<=amount;amt++){
+                int take = 1e9;
+                if(amt >= coins[ind]){
+                    take =1+ dp[ind][amt-coins[ind]];
+                }
+                int nottake = dp[ind-1][amt];
+                dp[ind][amt]= min(take,nottake);
+            }
+        }
+        if (dp[coins.size()-1][amount] >= 1e9){
             return -1;
         }
-        return ans;
+        return dp[coins.size()-1][amount];
     }
 };
