2882420da6168602475c403c4d74aa7d2a715723
Priyanshu Gupta <pg747974@gmail.com>
2026-07-22 22:49:26 +0530

    Time: 55 ms (69.97%) | Memory: 75.9 MB (63.90%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -16,7 +16,20 @@ public:
         return dp[left][right]= max(function(left+1,right,s,dp),function(left,right-1,s,dp));
     }
     int longestPalindromeSubseq(string s) {
-        vector<vector<int>> dp(s.size(),vector<int>(s.size(),-1));
-        return function(0,s.size()-1,s,dp);
+        vector<vector<int>> dp(s.size(),vector<int>(s.size(),0));
+        for(int i=0;i<s.size();i++){
+            dp[i][i]=1;
+        }
+        for(int left = s.size() - 1; left >= 0; left--){
+            for(int right = left + 1; right < s.size(); right++){
+                if(s[left]==s[right]){
+                    dp[left][right]= 2+ dp[left+1][right-1];
+                }
+                else{
+                    dp[left][right]= max(dp[left+1][right],dp[left][right-1]);
+                }
+            }
+        }
+        return dp[0][s.size()-1];
     }
 };
