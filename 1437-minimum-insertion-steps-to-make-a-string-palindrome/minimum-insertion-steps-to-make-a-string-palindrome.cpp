5ee2ab40f7f7317a5865d389dba173a2bee0611d
Priyanshu Gupta <pg747974@gmail.com>
2026-07-23 11:01:06 +0530

    Time: 27 ms (69.96%) | Memory: 31.5 MB (59.47%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public: 
+    int function(int left, int right,string & s,vector<vector<int>>& dp){
+        if(left>right){
+            return 0;
+        }
+        if(left==right){
+            return 0;
+        }
+        if(dp[left][right]!=-1){
+            return dp[left][right];
+        }
+        if(s[left]==s[right]){
+            return dp[left][right]= function(left+1,right-1,s,dp);
+        }
+        else{
+            return dp[left][right] = 1+min(function(left+1,right,s,dp),function(left,right-1,s,dp));
+        }
+    }
+    int minInsertions(string s) {
+        vector<vector<int>>dp(s.size(),vector<int>(s.size(),-1));
+        return function(0,s.size()-1,s,dp);
+    }
+};
