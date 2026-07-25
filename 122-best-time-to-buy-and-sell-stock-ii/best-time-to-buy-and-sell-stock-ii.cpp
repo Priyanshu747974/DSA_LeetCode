73d446e3d3b2a6c8046e71e1a4b2e42beb01382e
Priyanshu Gupta <pg747974@gmail.com>
2026-07-25 16:47:40 +0530

    Time: 8 ms (37.27%) | Memory: 24.8 MB (30.01%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -15,7 +15,20 @@ public:
         }
     }
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>>dp (prices.size(),vector<int>(2,-1));
-        return function(0,1,prices,dp);
+        vector<vector<int>>dp (prices.size()+1,vector<int>(2,-1));
+        for(int i=0;i<2;i++){
+            dp[prices.size()][i]=0;
+        }
+        for(int i=prices.size()-1;i>=0;i--){
+            for(int canbuy=0;canbuy<2;canbuy++){
+                if(canbuy==1){
+                    dp[i][canbuy] = max(-prices[i] + dp[i + 1][0],dp[i + 1][1]);
+                }
+                else{
+                    dp[i][canbuy] = max(prices[i] + dp[i + 1][1],dp[i + 1][0]);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
