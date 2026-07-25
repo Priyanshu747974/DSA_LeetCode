c4770b4c70bac0557c017aabd88ddfae04988b47
Priyanshu Gupta <pg747974@gmail.com>
2026-07-25 21:13:21 +0530

    Time: 3 ms (83.69%) | Memory: 17.7 MB (61.93%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int function(int ind, int tran, vector<int>& prices, int k, vector<vector<int>>& dp) {
+    int function(int ind, int tran, vector<int>& prices, int k,
+                 vector<vector<int>>& dp) {
         if (ind >= prices.size() || tran == 2 * k) {
             return 0;
         }
@@ -19,7 +20,25 @@ public:
         }
     }
     int maxProfit(int k, vector<int>& prices) {
-        vector<vector<int>> dp(prices.size(), vector<int>(2 * k, -1));
-        return function(0, 0, prices, k, dp);
+        vector<vector<int>> dp(prices.size()+1, vector<int>(2 * k+1, -1));
+        int n = prices.size();
+        for (int i = 0; i <= 2 * k; i++) {
+            dp[n][i] = 0;
+        }
+        for (int i = 0; i < n; i++) {
+            dp[i][2 * k] = 0;
+        }
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int tran = 0; tran < 2 * k; tran++) {
+                if (tran % 2 == 0) {
+                    dp[ind][tran] = max(-prices[ind] + dp[ind + 1][tran + 1],
+                                        dp[ind + 1][tran]);
+                } else {
+                    dp[ind][tran] = max(prices[ind] + dp[ind + 1][tran + 1],
+                                        dp[ind + 1][tran]);
+                }
+            }
+        }
+        return dp[0][0];
     }
 };
