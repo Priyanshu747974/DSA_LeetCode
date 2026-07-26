a2252c3291864e6d435c0d7051eb4bfd28022529
Priyanshu Gupta <pg747974@gmail.com>
2026-07-26 22:01:45 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (53.28%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,21 +1,43 @@
 class Solution {
 public:
-    int function(int ind, int canbuy, vector<int>& prices, vector<vector<int>>& dp){
-        if(ind >= prices.size()){
+    int function(int ind, int canbuy, vector<int>& prices,
+                 vector<vector<int>>& dp) {
+        if (ind >= prices.size()) {
             return 0;
         }
-        if(dp[ind][canbuy]!=-1){
+        if (dp[ind][canbuy] != -1) {
             return dp[ind][canbuy];
         }
-        if(canbuy==1){
-            return dp[ind][canbuy] = max(-prices[ind]+function(ind+1,0,prices,dp),function(ind+1,1,prices,dp));
-        }
-        else{
-            return dp[ind][canbuy] = max(prices[ind]+function(ind+2,1,prices,dp),function(ind+1,0,prices,dp));
+        if (canbuy == 1) {
+            return dp[ind][canbuy] =
+                       max(-prices[ind] + function(ind + 1, 0, prices, dp),
+                           function(ind + 1, 1, prices, dp));
+        } else {
+            return dp[ind][canbuy] =
+                       max(prices[ind] + function(ind + 2, 1, prices, dp),
+                           function(ind + 1, 0, prices, dp));
         }
     }
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>> dp(prices.size(),vector<int>(2,-1));
-        return function(0,1,prices,dp);
+        int n = prices.size();
+        vector<vector<int>> dp(n+2, vector<int>(2, -1));
+        for (int i = 0; i < 2; i++) {
+            dp[n+1][i] = 0;
+            dp[n][i]=0;
+        }
+        for (int ind = n-1; ind >= 0; ind--) {
+            for (int canbuy = 0; canbuy < 2; canbuy++) {
+                if (canbuy == 1) {
+                    dp[ind][canbuy] = max(
+                               -prices[ind] + dp[ind + 1][0],
+                               dp[ind + 1][1]);
+                } else {
+                    dp[ind][canbuy] = max(
+                               prices[ind] + dp[ind + 2][1],
+                               dp[ind + 1][0]);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
