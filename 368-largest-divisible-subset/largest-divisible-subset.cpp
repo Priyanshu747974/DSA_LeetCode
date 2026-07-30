1049666cd361ecceb2ef329e6ad9eb4359c20ecb
Priyanshu Gupta <pg747974@gmail.com>
2026-07-30 20:35:30 +0530

    Time: 15 ms (27.26%) | Memory: 12.7 MB (51.75%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,48 +1,33 @@
 class Solution {
 public:
-    int function(int ind, int prev, vector<int>& nums,
-                 vector<vector<int>>& dp) {
-        if (ind >= nums.size()) {
-            return 0;
-        }
-        if (dp[ind][prev + 1] != -1) {
-            return dp[ind][prev + 1];
-        }
-        int notTake = function(ind + 1, prev, nums, dp);
-
-        int take = 0;
-        if (prev == -1 || nums[ind] % nums[prev] == 0) {
-            take = 1 + function(ind + 1, ind, nums, dp);
-        }
-
-        return dp[ind][prev + 1] = max(take, notTake);
-    }
     vector<int> largestDivisibleSubset(vector<int>& nums) {
+        int LIS = 1;
+        int lastIndex = 0;
         sort(nums.begin(), nums.end());
-        int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        function(0, -1, nums, dp);
-        vector<int> ans;
-
-        int ind = 0;
-        int prev = -1;
-
-        while (ind < n) {
-            int notTake = function(ind + 1, prev, nums, dp);
-
-            int take = -1;
-            if (prev == -1 || nums[ind] % nums[prev] == 0) {
-                take = 1 + function(ind + 1, ind, nums, dp);
-            }
-
-            if (take >= notTake) {
-                ans.push_back(nums[ind]);
-                prev = ind;
-            }
-
-            ind++;
+        vector<int> dp(nums.size(), 1);
+        vector<int> hash(nums.size());
+        for (int i = 0; i < nums.size(); i++) {
+            hash[i] = i;
         }
-
+        for (int ind = 0; ind < nums.size(); ind++) {
+            for (int prev = 0; prev < ind; prev++) {
+                if (nums[ind] % nums[prev] == 0 && dp[prev] + 1 > dp[ind]) {
+                    dp[ind] = dp[prev] + 1;
+                    hash[ind] = prev;
+                }
+            }
+            if (dp[ind] > LIS) {
+                LIS = dp[ind];
+                lastIndex = ind;
+            }
+        }
+        vector<int> ans;
+        ans.push_back(nums[lastIndex]);
+        while(hash[lastIndex]!=lastIndex){
+            lastIndex = hash[lastIndex];
+            ans.push_back(nums[lastIndex]);
+        }
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
