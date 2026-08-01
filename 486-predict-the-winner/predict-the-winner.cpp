04f504616eeb4523e1d1b8f8949c3c848bb841c4
Priyanshu Gupta <pg747974@gmail.com>
2026-08-01 20:45:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (45.39%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    int function(int i,int j,vector<int>& nums){
+    int function(int i,int j,vector<int>& nums, vector<vector<int>>& dp){
         int n = j-i+1;
         if(n==1){
             return nums[i];
         }
-        int takeleft = nums[i] - function(i+1,j,nums);
-        int takeright = nums[j] - function(i,j-1,nums);
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int takeleft = nums[i] - function(i+1,j,nums,dp);
+        int takeright = nums[j] - function(i,j-1,nums,dp);
 
-      return max(takeleft,takeright);
+      return dp[i][j]=max(takeleft,takeright);
     }
     bool predictTheWinner(vector<int>& nums) {
-        int diff = function(0,nums.size()-1,nums);
+        vector<vector<int>> dp(nums.size(),vector<int>(nums.size(),-1));
+        int diff = function(0,nums.size()-1,nums,dp);
         if(diff>=0){
             return true;
         }
