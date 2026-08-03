137dbf2a41f7ca9d1439a6f3078658446e3fe251
Priyanshu Gupta <pg747974@gmail.com>
2026-08-03 23:56:06 +0530

    Time: 8 ms (9.32%) | Memory: 12.3 MB (7.31%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -4,12 +4,12 @@ public:
         if(x<0){
             return false;
         }
-        vector<int> temp(1,0);
+        if(x == 0) return true;
+        vector<int> temp;
         while(x>0){
             temp.push_back(x%10);
             x = x/10;
         }
-        temp.push_back(0);
         int i =0;
         int j =temp.size()-1;
         while(j>i){
