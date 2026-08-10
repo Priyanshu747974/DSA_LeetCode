849247a73fda3fe7af20827748a97241fdd815ee
Priyanshu Gupta <pg747974@gmail.com>
2026-08-10 10:33:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (40.26%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int function(int i){
-        vector<int> f(38,0);
-        f[0]=0;
-        f[1]=1;
-        f[2]=1;
-        for(int i=0;i<35;i++){
-            f[i+3]=f[i]+f[i+1]+f[i+2];
+    int function(int i,vector<int>& dp){
+        if(i==0){
+            return 0;
         }
-        return f[i];
+        if(i==1 || i==2){
+            return 1;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int current = function(i-1,dp)+function(i-2,dp)+function(i-3,dp);
+        return dp[i]=current;
     }
     int tribonacci(int n) {
-        return function(n);
+        vector<int> dp(n+1,-1);
+        return function(n,dp);
     }
 };
