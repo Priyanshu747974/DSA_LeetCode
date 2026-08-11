20304a06be821ece81a8cc74a74a062029f17005
Priyanshu Gupta <pg747974@gmail.com>
2026-08-11 12:14:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (5.02%) - LeetSync

--- 392-is-subsequence/is-subsequence.cpp
+++ 392-is-subsequence/is-subsequence.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
-    int function(int i, int j,string & s, string &t,vector<vector<int>>& dp){
-        if(i<0 || j<0){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s[i]==t[j]){
-            return dp[i][j]=1+function(i-1,j-1,s,t,dp);
-        }
-        return  dp[i][j]=max(function(i-1,j,s,t,dp),function(i,j-1,s,t,dp));
-    }
     bool isSubsequence(string s, string t) {
-        vector<vector<int>> dp(s.size(),vector<int>(t.size(),-1));
-        int lcs = function(s.size()-1,t.size()-1,s,t,dp);
-        if(lcs == s.size()){
+        vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
+        for (int i = 1; i <= s.size(); i++) {
+            for (int j = 1; j <= t.size(); j++) {
+                if (s[i-1] == t[j-1]) {
+                     dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else{
+                    dp[i][j] = max(dp[i - 1][j],dp[i][j - 1]);
+                }
+            }
+        }
+        if(s.size()==dp[s.size()][t.size()]){
             return true;
         }
         return false;
