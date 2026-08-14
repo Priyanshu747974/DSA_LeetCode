44facfecdb9019c7b36f4dd330f6419660b623c2
Priyanshu Gupta <pg747974@gmail.com>
2026-08-14 22:23:50 +0530

    Time: 4 ms (28.78%) | Memory: 9.5 MB (57.72%) - LeetSync

--- 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
+++ 3349-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -4,15 +4,15 @@ public:
         unordered_map<char,int> mpp;
         int i =0;
         int j =0;
-        int ans = j-i;
+        int ans = j-i+1;
         while(j<s.size() && i<=j){
             mpp[s[j]]++;
             while(mpp[s[j]]>2){
                 mpp[s[i]]--;
                 i++;
             }
+            ans= max(j-i+1,ans);
             j++;
-            ans= max(j-i,ans);
         }
         return ans;
     }
