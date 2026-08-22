65597591b4373a020dc549aec7c88b4f6a624660
Priyanshu Gupta <pg747974@gmail.com>
2026-08-22 20:44:08 +0530

    Time: 3 ms (86.86%) | Memory: 262 MB (52.54%) - LeetSync

---
+++ 3355-minimum-levels-to-gain-more-points/minimum-levels-to-gain-more-points.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumLevels(vector<int>& possible) {
+        int total = 0;
+        for(int i =0;i<possible.size();i++){
+            if(possible[i]==0){
+                possible[i]=-1;
+            }
+            total = total+possible[i];
+        }
+        int alice = 0;
+        int bob = 0;
+        int i = 0;
+        while(i<possible.size()-1){
+            alice = alice + possible[i];
+            bob = total - alice;
+            if(alice>bob){
+              return i+1;  
+              break;
+            }
+            i++;
+        }
+       return -1;
+    }
+};
