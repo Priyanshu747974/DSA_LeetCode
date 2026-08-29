a53af761cca9d2f0439fc83d93c000d53fd06ec2
Priyanshu Gupta <pg747974@gmail.com>
2026-08-29 10:59:34 +0530

    Time: 316 ms (52.71%) | Memory: 0B (100.00%) - LeetSync

--- 595-big-countries/big-countries.sql
+++ 595-big-countries/big-countries.sql
@@ -1,4 +1,20 @@
 # Write your MySQL query statement below
+
+#SELECT name, population, area
+#FROM World
+#WHERE area>=3000000 OR population >= 25000000;
+
 SELECT name, population, area
 FROM World
-WHERE area>=3000000 OR population >= 25000000;
+WHERE area >= 3000000
+
+UNION
+
+SELECT name, population, area
+FROM World
+WHERE population >= 25000000;
+
+#Why use UNION instead of OR?
+#Index Optimization: When indexes exist on area and population, using OR can sometimes force SQL engines to perform a full table scan.
+
+#Independent Scans: Using UNION allows the database engine to perform two fast index lookups independently and combine the distinct results, which often performs better on large tables.
