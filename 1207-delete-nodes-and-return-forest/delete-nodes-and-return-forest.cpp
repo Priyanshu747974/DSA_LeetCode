98e15cef1ee13d9d23db721846fe98de3ac4c882
Priyanshu Gupta <pg747974@gmail.com>
2026-08-30 12:26:58 +0530

    Time: 14 ms (66.67%) | Memory: 27.3 MB (28.41%) - LeetSync

---
+++ 1207-delete-nodes-and-return-forest/delete-nodes-and-return-forest.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    vector<TreeNode*> ans;
+    unordered_set<int> st;
+
+    TreeNode* solve(TreeNode* root) {
+
+        if(root == NULL)
+            return NULL;
+
+        root->left = solve(root->left);
+        root->right = solve(root->right);
+
+        if(st.count(root->val)) {
+
+            if(root->left)
+                ans.push_back(root->left);
+
+            if(root->right)
+                ans.push_back(root->right);
+
+            return NULL;
+        }
+
+        return root;
+    }
+
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+
+        for(int x : to_delete)
+            st.insert(x);
+
+        root = solve(root);
+        if(root)
+            ans.push_back(root);
+
+        return ans;
+    }
+};
