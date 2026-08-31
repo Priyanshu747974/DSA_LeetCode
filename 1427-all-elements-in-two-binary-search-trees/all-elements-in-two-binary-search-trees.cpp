8f922f53002cf9b148118b56b7674f9dfa39894b
Priyanshu Gupta <pg747974@gmail.com>
2026-08-31 13:04:21 +0530

    Time: 0 ms (100.00%) | Memory: 88.5 MB (39.62%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.cpp
@@ -15,15 +15,30 @@ public:
         if(root==NULL){
             return;
         }
-        v.push_back(root->val);
+        
         function(root->left,v);
+        v.push_back(root->val);
         function(root->right,v);
     }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int> ans;
-        function(root1,ans);
-        function(root2,ans);
-        sort(ans.begin(),ans.end());
-        return ans;
+        vector<int> list1, list2;
+        function(root1, list1);
+        function(root2, list2);
+
+        vector<int> result;
+        int i = 0, j = 0;
+        int n1 = list1.size(), n2 = list2.size();
+
+        while (i < n1 && j < n2) {
+            if (list1[i] <= list2[j]) {
+                result.push_back(list1[i++]);
+            } else {
+                result.push_back(list2[j++]);
+            }
+        }
+        while (i < n1) result.push_back(list1[i++]);
+        while (j < n2) result.push_back(list2[j++]);
+
+        return result;
     }
 };
