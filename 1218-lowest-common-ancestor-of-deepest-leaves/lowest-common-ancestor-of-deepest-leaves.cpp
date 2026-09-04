dd431e329cdb4869ddae020fef29aa25f1a8261b
Priyanshu Gupta <pg747974@gmail.com>
2026-09-04 11:59:54 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (85.02%) - LeetSync

---
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<int, TreeNode*> dfs(TreeNode* root) {
+        if (!root)
+            return {0, nullptr};
+
+        auto left = dfs(root->left);
+        auto right = dfs(root->right);
+
+        int leftDepth = left.first;
+        int rightDepth = right.first;
+
+        // Equal depths mean current node bridges the deepest leaves
+        if (leftDepth == rightDepth) {
+            return {leftDepth + 1, root};
+        }
+
+        // Deeper side holds the LCA candidate
+        if (leftDepth > rightDepth) {
+            return {leftDepth + 1, left.second};
+        } else {
+            return {rightDepth + 1, right.second};
+        }
+    }
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        return dfs(root).second;
+    }
+};
