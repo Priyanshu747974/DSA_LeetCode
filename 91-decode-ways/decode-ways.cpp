94ad42e7d8f0bebb7115252f06faedd17508cacc
Priyanshu Gupta <pg747974@gmail.com>
2026-09-14 10:25:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (63.92%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,25 +1,39 @@
 class Solution {
 public:
-    int solve(string &s, int i,vector<int> &dp){
-        if(i==s.size()){
-            return 1;
-        }
-        if(s[i]=='0'){
-            return 0;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        int result = solve(s,i+1,dp);
-        if(i<s.size()-1){
-            if(s[i]=='1'|| (s[i]=='2'&& s[i+1]<='6')){
-                result += solve(s,i+2,dp);
+    /*
+        int solve(string& s, int i, vector<int>& dp) {
+            if (i == s.size()) {
+                return 1;
+            }
+            if (s[i] == '0') {
+                return 0;
+            }
+            if (dp[i] != -1) {
+                return dp[i];
+            }
+            int result = solve(s, i + 1, dp);
+            if (i < s.size() - 1) {
+                if (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6')) {
+                    result += solve(s, i + 2, dp);
+                }
+            }
+            return dp[i] = result;
+        }*/
+    int numDecodings(string s) {
+        vector<int> dp(s.size()+1, 0);
+        dp[s.size()]=1;
+        for (int i = s.size() - 1; i >= 0; i--) {
+            if (s[i] == '0') {
+                dp[i] = 0;
+            } else {
+                dp[i] = dp[i + 1];
+                if (i < s.size() - 1) {
+                    if (s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6')) {
+                        dp[i] += dp[i + 2];
+                    }
+                }
             }
         }
-        return dp[i]=result;
-    }
-    int numDecodings(string s) {
-        vector<int> dp(s.size(),-1);
-        return solve(s,0,dp);
+        return dp[0];
     }
 };
