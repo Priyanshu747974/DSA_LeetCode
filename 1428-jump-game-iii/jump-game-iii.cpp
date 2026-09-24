10fe490a914580df021b230887670d99c24ab362
Priyanshu Gupta <pg747974@gmail.com>
2026-09-24 16:29:29 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (44.75%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>& adj, int node,vector<int>& visited,vector<int>& arr){
-        if(arr[node]==0){
+    bool dfs(vector<int>& arr, int node, vector<int>& visited) {
+        if (arr[node] == 0)
             return true;
+
+        visited[node] = 1;
+
+        int n = arr.size();
+
+        int right = node + arr[node];
+        if (right < n && !visited[right]) {
+            if (dfs(arr, right, visited))
+                return true;
         }
-        
-        visited[node]=1;
-        for(auto it : adj[node]){
-            if(visited[it]==0){
-                if (dfs(adj, it, visited, arr)) {
-                    return true;
-                }
-            }
+
+        int left = node - arr[node];
+        if (left >= 0 && !visited[left]) {
+            if (dfs(arr, left, visited))
+                return true;
         }
+
         return false;
     }
+
     bool canReach(vector<int>& arr, int start) {
-        vector<vector<int>> adj;
-        for(int i =0;i<arr.size();i++){
-            vector<int> list;
-            if(i+arr[i]<arr.size()){
-                list.push_back(i+arr[i]);
-            }
-            if(i-arr[i]>=0){
-                list.push_back(i-arr[i]);
-            }
-            adj.push_back(list);
-        }
-        vector<int> visited(arr.size(),0);
-        return dfs(adj,start,visited,arr);
+        vector<int> visited(arr.size(), 0);
+        return dfs(arr, start, visited);
     }
 };
